4e58558cf383a34a52d9c5e2c7b438c40363a965
Ivan Zulyan Pratama <zulyanivan11@gmail.com>
2023-10-08 18:19:54 +0700

    calculate

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-// funciton create for calculate the BMI
 calculateBMI = () => {
-  // get height and weight input values
   const height = document.getElementById("heightInput").value;
   const weight = document.getElementById("weightInput").value;
 
-  // validate input
+  // Validasi Input
   if (height === "" || weight === "") {
-    alert("Silahkan Masukkan Tinggi dan Berat Badan Anda");
+    alert("Please Enter Your Height and Weight");
     return;
   }
 
-  // calculate BMI
   let bmi = weight / (height / 100) ** 2;
-  let result = "BMI Anda " + bmi.toFixed(2);
+  let result = "Your BMI is " + bmi.toFixed(2) + " which means you are";
 
   let category = "";
 
-  // determine BMI category
+  // Kategori BMI
   if (bmi < 18.5) {
-    category = "Berat Badan Kurang";
-  } else if (bmi >= 18.5 && bmi < 25) {
-    category = "Berat Badan Normal";
-  } else if (bmi >= 25 && bmi < 30) {
-    category = "Berat Badan Lebih";
+    category = "Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    category = "Normal Weight";
+  } else if (bmi >= 25 && bmi < 29.9) {
+    category = "Overweight";
   } else {
-    category = "Obesitas";
+    category = "Obesity";
   }
 
-  result += "<br> Kategori: " + category;
+  result += "<br> Category: " + category;
 
-  // display result
+  // Menampilkan Hasil
   document.getElementById("result").innerHTML = result;
 };
